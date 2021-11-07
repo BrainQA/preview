e9eac3c0ef69c02d805e4626f58472cbb16322ea
Taras <taras.pazzyak@gmail.com>
2021-11-06 23:31:42 -0700

    added examples

--- js/cycles.js
+++ js/cycles.js
@@ -13,11 +13,41 @@ let num = 50;
 // }
 // while (num < 55);
 
-for(let i = 1; i < 10; i++) {
-    if(i === 6) {
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
         // break;
         continue;
     }
 
     console.log(i);
 }
+
+// let num = 0;
+
+// while (num < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//         num++;
+//     } else {
+//         console.log("error");
+//         num--;
+//     }
+// }
+
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//         num++;
+//     } else {
+//         console.log("error");
+//         num--;
+//     }
+// } while (num < 2);
